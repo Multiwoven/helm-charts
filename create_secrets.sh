e42be12316062fde00eee315638a8e7453914116
AngeloAISquared <angelo.bradley@squared.ai>
2024-05-08 15:51:14 -0400

    fix: added optional imagePullSecrets for private registry access and imagePullPolicy to explicitly enforce pulling images from the repo

--- create_secrets.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete secret temporal-cloud -n multiwoven
-
-kubectl create secret generic temporal-cloud -n multiwoven \
-    --from-file=temporal-root-cert=./temporal.pem \
-    --from-file=temporal-client-key=./temporal.key
